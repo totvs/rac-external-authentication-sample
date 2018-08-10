2f0912f731200a2780b9900ce17aabd3e1a71965
Flavio Campelo <flaviocampelo@hotmail.com>
2018-08-10 12:16:45 -0300

    -APIs para exemplificar uma autenticação do RAC realizada externamente pelo ERP

---
+++ RacExternalAuthSample/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace RacExternalAuthSample.Controllers
+{
+  [Route("rest/v1/[controller]")]
+  [AllowAnonymous]
+  public class UsersController : Controller
+  {
+    /// <summary>
+    /// Busca o identificador do usuário
+    /// </summary>
+    /// <param name="username">Nome do usuário</param>
+    /// <returns>Código identificador do usuário</returns>
+    [HttpGet("id/{username}")]
+    public Stream GetUserId(string username) =>
+      username == "test" ?
+        new MemoryStream(Encoding.UTF8.GetBytes("1001")) :
+        null;
+
+    /// <summary>
+    /// Valida as credenciais do usuário
+    /// </summary>
+    /// <returns>Retorna true se as credenciais foram validadas pelo ERP</returns>
+    [HttpPost("validate")]
+    public bool Validate()
+    {
+      using (StreamReader reader = new StreamReader(this.HttpContext.Request.Body, Encoding.UTF8))
+      {
+        string[] dataParsed = reader.ReadToEnd().Split('&');
+
+        string username = dataParsed[0].Split('=')[1];
+        string password = dataParsed[1].Split('=')[1];
+
+        return username == "test" && password == "test@123";
+      }
+    }
+  }
+}
